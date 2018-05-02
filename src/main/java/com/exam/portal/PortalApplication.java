92ba2e06fe82f54b1ff5a23ae2cd1fa0a216ea34
satyabunny <satyabunny84@gmail.com>
2018-05-02 20:14:24 +0530

    spring initializer

--- src/main/java/com/exam/portal/PortalApplication.java
+++ src/main/java/com/exam/portal/PortalApplication.java
@@ -1,13 +1,22 @@
 package com.exam.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.exam.portal")
-public class PortalApplication {
+public class PortalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortalApplication.class, args);
-	}
+    private static Class<PortalApplication> application = PortalApplication.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(application, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(application);
+    }
 }
